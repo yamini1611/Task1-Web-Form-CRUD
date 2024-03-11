6b54fd2988de9d0069b4611f01d82a743e65cfe0
yaminijcgvak <yamini.j@cgvakindia.com>
2024-03-11 19:26:21 +0530

    Add project files.

---
+++ Task1 Web Form/DynamicTableCRUD.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DynamicTableCRUD.aspx.cs" Inherits="WebFormsApplication.DynamicCRUD" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Dynamic CRUD</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        h2 {
+            margin-bottom: 10px;
+        }
+        .form-group {
+            margin-bottom: 10px;
+        }
+        label {
+            font-weight: bold;
+        }
+        .btn {
+            padding: 5px 10px;
+            background-color: #007bff;
+            border: none;
+            color: #fff;
+            cursor: pointer;
+        }
+        .btn:hover {
+            background-color: #0056b3;
+        }
+        .table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        .table th, .table td {
+            padding: 8px;
+            border-bottom: 1px solid #ddd;
+            text-align: left;
+        }
+        .table th {
+            background-color: #007bff;
+            color: #fff;
+        }
+        .table tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        .enclose{
+            border:solid;
+            padding:50px;
+        }
+    </style>
+</head>
+<body>
+    <div class="enclose">
+        <h1 class="text-center">Task 1 CRUD</h1>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Add Record</h2>
+            <div class="form-group">
+                <label for="txtAddName">Name:</label>
+                <asp:TextBox ID="txtAddName" runat="server"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label for="txtAddAge">Age:</label>
+                <asp:TextBox ID="txtAddAge" runat="server" TextMode="Number"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <asp:Button ID="btnAdd" runat="server" Text="Add Record" CssClass="btn" OnClick="btnAdd_Click" />
+            </div>
+        </div>
+        <hr />
+        <div>
+            <h2>Employee Records</h2>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="ID" 
+                OnRowEditing="GridView1_RowEditing" OnRowCancelingEdit="GridView1_RowCancelingEdit" 
+                OnRowUpdating="GridView1_RowUpdating" OnRowDeleting="GridView1_RowDeleting" 
+                OnRowDataBound="GridView1_RowDataBound" CssClass="table">
+                <Columns>
+                    <asp:BoundField DataField="ID" HeaderText="ID" ReadOnly="True" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="Age" HeaderText="Age" />
+                    <asp:CommandField ShowEditButton="True" ShowDeleteButton="True" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+            </div>
+
+</body>
+</html>
---
+++ Task1 Web Form/DynamicTableCRUD.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace WebFormsApplication
+{
+    public partial class DynamicCRUD : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+
+        protected void BindGrid()
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT ID, Name, Age FROM Employee", con))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        GridView1.DataSource = dt;
+                        GridView1.DataBind();
+                    }
+                }
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            string name = txtAddName.Text;
+            int age = Convert.ToInt32(txtAddAge.Text);
+
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Employee (Name, Age) VALUES (@Name, @Age)", con))
+                {
+                    cmd.Parameters.AddWithValue("@Name", name);
+                    cmd.Parameters.AddWithValue("@Age", age);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            txtAddName.Text = "";
+            txtAddAge.Text = "";
+            BindGrid();
+        }
+
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+            string name = e.NewValues["Name"].ToString();
+            int age = Convert.ToInt32(e.NewValues["Age"]);
+
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("UPDATE Employee SET Name = @Name, Age = @Age WHERE ID = @ID", con))
+                {
+                    cmd.Parameters.AddWithValue("@Name", name);
+                    cmd.Parameters.AddWithValue("@Age", age);
+                    cmd.Parameters.AddWithValue("@ID", id);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            GridView1.EditIndex = -1;
+            BindGrid();
+        }
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM Employee WHERE ID = @ID", con))
+                {
+                    cmd.Parameters.AddWithValue("@ID", id);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            BindGrid();
+        }
+
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            BindGrid();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            BindGrid();
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow && (e.Row.RowState & DataControlRowState.Edit) == DataControlRowState.Edit)
+            {
+                TextBox txtEditName = (TextBox)e.Row.FindControl("txtEditName");
+                TextBox txtEditAge = (TextBox)e.Row.FindControl("txtEditAge");
+                Label lblName = (Label)e.Row.FindControl("lblName");
+                Label lblAge = (Label)e.Row.FindControl("lblAge");
+
+                if (txtEditName != null && txtEditAge != null && lblName != null && lblAge != null)
+                {
+                    txtEditName.Text = lblName.Text;
+                    txtEditAge.Text = lblAge.Text;
+                }
+            }
+
+        }
+    }
+}
